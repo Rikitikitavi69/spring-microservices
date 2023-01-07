233e40d60f8364974ef907c70f35ac2c31fdd737
Sergey Lazarenko <rikitikitavi69@gmail.com>
2023-01-07 12:13:29 +0300

    ch11: added https

--- spring-cloud/eureka-server/src/test/java/se/magnus/springcloud/eurekaserver/EurekaServerApplicationTests.java
+++ spring-cloud/eureka-server/src/test/java/se/magnus/springcloud/eurekaserver/EurekaServerApplicationTests.java
@@ -5,6 +5,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
@@ -13,28 +14,36 @@ import org.springframework.http.ResponseEntity;
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class EurekaServerApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+    @Test
+    void contextLoads() {
+    }
 
-  @Autowired
-  private TestRestTemplate testRestTemplate;
+    @Value("${app.eureka-username}")
+    private String username;
 
-  @Test
-  void catalogLoads() {
+    @Value("${app.eureka-password}")
+    private String password;
 
-    String expectedReponseBody = "{\"applications\":{\"versions__delta\":\"1\",\"apps__hashcode\":\"\",\"application\":[]}}";
-    ResponseEntity<String> entity = testRestTemplate.getForEntity("/eureka/apps", String.class);
-    assertEquals(HttpStatus.OK, entity.getStatusCode());
-    assertEquals(expectedReponseBody, entity.getBody());
-  }
+    private TestRestTemplate testRestTemplate;
 
-  @Test
-  void healthy() {
-    String expectedReponseBody = "{\"status\":\"UP\"}";
-    ResponseEntity<String> entity = testRestTemplate.getForEntity("/actuator/health", String.class);
-    assertEquals(HttpStatus.OK, entity.getStatusCode());
-    assertEquals(expectedReponseBody, entity.getBody());
-  }
+    @Autowired
+    void setTestRestTemplate(TestRestTemplate testRestTemplate) {
+        this.testRestTemplate = testRestTemplate.withBasicAuth(username, password);
+    }
+
+    @Test
+    void catalogLoads() {
+        String expectedReponseBody = "{\"applications\":{\"versions__delta\":\"1\",\"apps__hashcode\":\"\",\"application\":[]}}";
+        ResponseEntity<String> entity = testRestTemplate.getForEntity("/eureka/apps", String.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertEquals(expectedReponseBody, entity.getBody());
+    }
+
+    @Test
+    void healthy() {
+        String expectedReponseBody = "{\"status\":\"UP\"}";
+        ResponseEntity<String> entity = testRestTemplate.getForEntity("/actuator/health", String.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+        assertEquals(expectedReponseBody, entity.getBody());
+    }
 }
-
